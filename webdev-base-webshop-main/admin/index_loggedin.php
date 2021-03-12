5f1e276c6c15c386cd992e5a10d52a06345209ee
Wessel van den Ijssel <wessel069@gmail.com>
2021-03-12 09:28:41 +0100

    logout

--- webdev-base-webshop-main/admin/index_loggedin.php
+++ webdev-base-webshop-main/admin/index_loggedin.php
@@ -6,6 +6,7 @@
     <li><a href="users/">Gebruikers</a></li>
     <li><a href="orders/">Bestellingen</a></li>
     <li><a href="producten">Producten</a></li>
+    <a href="logout.php">logout</a>
 </ul>
 <?php
     include('core/footer.php');
