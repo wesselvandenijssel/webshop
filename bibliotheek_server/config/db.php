3050b9d03131b22269456b4c58d6e83e0b4e2d38
Wessel van den Ijssel <wessel069@gmail.com>
2021-11-12 10:21:08 +0100

    resolve

--- bibliotheek_server/config/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("DB_HOST", "localhost");
-define("DB_NAME", "u200517_bibliotheek");
-define("DB_USER", "u200517_bibli");
-define("DB_PASS", "0D7d3Yse8J");
--- database5/logout.php
+++
@@ -1,8 +0,0 @@
-<!--
-Into this file, we write a code for logout.
--->
-<?php
-session_start();
-session_destroy();
-header("Location: login.php");
-?>
